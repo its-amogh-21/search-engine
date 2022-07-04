77676e1de10d9e33c92a1ced0f2bc3beaf6afbdc
its-amogh-21 <auuchavan@gmail.com>
2022-07-04 16:20:46 +0530

    tailwind css config

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const App = () => {
     return(
-        <div>Welcome to App</div>
+        <h1 className="text-3xl font-bold underline text-sky-400">
+        Hello world!
+        </h1>
     )
 }
 
